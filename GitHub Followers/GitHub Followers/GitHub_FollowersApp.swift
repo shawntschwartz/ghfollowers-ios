be75f1af5ec591c1536b0093755931544518a250
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-01-23 22:29:19 -0800

    init empty xcode project

---
+++ GitHub Followers/GitHub Followers/GitHub_FollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_FollowersApp.swift
+//  GitHub Followers
+//
+//  Created by shawn on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_FollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
